3b03ffe3eeafc9d3b597936b99d61fe4847ccfc2
Brandon Chen <brandonkachen@Brandons-MacBook-Pro.local>
2024-09-08 11:26:14 -0700

    fix: filePath

--- src/pages/api/repos/[repoId]/files/[...filePath].ts
+++ src/pages/api/repos/[repoId]/files/[...filePath].ts
@@ -1,38 +1,41 @@
-import { NextApiRequest, NextApiResponse } from "next"
-import { readFile } from "fs/promises"
-import path from "path"
-import { getRepoById } from "@/backend/db/queries"
+import { NextApiRequest, NextApiResponse } from "next";
+import { readFile } from "fs/promises";
+import path from "path";
+import { getRepoById } from "@/backend/db/queries";
+import { match, P } from "ts-pattern";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "GET") {
-    return res.status(405).json({ error: "Method not allowed" })
-  }
+  console.log("req.query", req.query);
+  return match(req)
+    .with(
+      {
+        method: "GET",
+        query: { repoId: P.string, filePath: P.array(P.string) },
+      },
+      async (getReq) => {
+        const repoId = parseInt(getReq.query.repoId);
+        const filePath = getReq.query.filePath;
+        try {
+          const repo = await getRepoById(Number(repoId));
+          if (!repo) {
+            return res.status(404).json({ error: "Repository not found" });
+          }
 
-  const { repoId, filePath } = req.query
-  console.log("repoId", repoId, "filePath", filePath)
+          const fullPath = path.join(repo.local_path, ...filePath);
+          console.log("fullPath", fullPath);
+          const fileContent = await readFile(fullPath, "utf-8");
 
-  if (!repoId || !filePath) {
-    return res.status(400).json({ error: "Missing repoId or filePath" })
-  }
-
-  try {
-    const repo = await getRepoById(Number(repoId))
-    if (!repo) {
-      return res.status(404).json({ error: "Repository not found" })
-    }
-
-    const fullPath = path.join(
-      repo.local_path,
-      ...(Array.isArray(filePath) ? filePath : [filePath])
+          res.status(200).json({ content: fileContent });
+        } catch (error) {
+          console.error("Error reading file:", error);
+          res.status(500).json({ error: "Error reading file" });
+        }
+      }
     )
-    const fileContent = await readFile(fullPath, "utf-8")
-
-    res.status(200).json({ content: fileContent })
-  } catch (error) {
-    console.error("Error reading file:", error)
-    res.status(500).json({ error: "Error reading file" })
-  }
+    .otherwise(() => {
+      res.status(405).json({ error: "invalid method or request structure" });
+    });
 }
