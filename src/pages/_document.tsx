5e647438da1e71626ab5abafebb2aeed8ffbd32b
James Grugett <jahooma@gmail.com>
2024-09-08 17:22:43 -0700

    black bg

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="antialiased">
+      <body className="antialiased bg-black">
         <Main />
         <NextScript />
       </body>
